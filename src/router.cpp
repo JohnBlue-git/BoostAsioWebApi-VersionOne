6810a9d6502d20b1dbec6b41006be4773fd4f889
JohnBlue-git <83207141+JohnBlue-git@users.noreply.github.com>
2025-04-03 08:33:39 +0000

    re-formating ...

--- src/router.cpp
+++ src/router.cpp
@@ -7,37 +7,37 @@ bool Router::isURL(const std::string &URL) {
 }
 
 std::string Router::removeId(const std::string& URL) {
-    std::regex pattern(R"(^(/[^/]+(?:/[^/]+)?)(?:/\d+)?$)");
-    return std::regex_replace(URL, pattern, "$1");
+  std::regex pattern(R"(^(/[^/]+(?:/[^/]+)?)(?:/\d+)?$)");
+  return std::regex_replace(URL, pattern, "$1");
 }
 
 void Router::addRoute(const std::string &URL, std::shared_ptr<IController> controller) {
   const std::string subURL = removeId(URL);
   if ( URL.compare(subURL) != 0 || false == isURL(subURL) ) {
-		perror("registed URL format incorrect.");
-		return;
+    perror("registed URL format incorrect.");
+    return;
   }
   else if ( true == Router::contains(subURL) ) {
-		perror("registed URL already exist.");
-		return;
+    perror("registed URL already exist.");
+    return;
   }
   routes[subURL] = controller;
 }
 
 bool Router::contains(const std::string &URL) {
   if ( false == isURL(URL) ) {
-		return false;
+    return false;
   }
-	return routes.contains(removeId(URL));
+  return routes.contains(removeId(URL));
 }
 
 std::shared_ptr<IController> Router::getController(const std::string &URL) {
   const std::string subURL = removeId(URL);
   if ( false == isURL(subURL) ) {
-		return nullptr;
+    return nullptr;
   }
-	if ( false == routes.contains(subURL) ) {
-		return nullptr;
-	}
-	return routes[subURL];
+  if ( false == routes.contains(subURL) ) {
+    return nullptr;
+  }
+  return routes[subURL];
 }

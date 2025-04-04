81dd4f4398dcb660aaf1c66e9cf4fbf70dc8dc2a
JohnBlue-git <83207141+JohnBlue-git@users.noreply.github.com>
2025-04-04 09:49:46 +0000

    refactor contoller (not yet finished ...)

--- tests/test_api_person.py
+++ tests/test_api_person.py
@@ -45,12 +45,20 @@ def test_get_person_by_id_not_found():
     assert response.status_code == 404
 
 @pytest.mark.order(5)
+def test_post_person():
+    url = f"{BASE_URL}{PERSON_API}/0"
+    payload = {'name': 'John Blue', 'age': 18}
+    headers = {'Content-Type': 'application/json'}
+    response = requests.patch(url, json=payload, headers=headers)
+    assert response.status_code == 200
+
+@pytest.mark.order(6)
 def test_delete_person_by_id():
     url = f"{BASE_URL}{PERSON_API}/0"
     response = requests.delete(url)
     assert response.status_code == 204
 
-@pytest.mark.order(6)
+@pytest.mark.order(7)
 def test_not_found():
     url = f"{BASE_URL}/42"
     response = requests.get(url)
